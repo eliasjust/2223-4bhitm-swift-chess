06710d8b8bd0d6e262d9460f8529c1abc131ca54
AL SABAGH Abdulrahman <it190204@WS8A01.htl-leonding.ac.at>
2023-03-15 08:54:36 +0100

    add board componenet

--- code/Sources/ContentView.swift
+++ code/Sources/ContentView.swift
@@ -6,17 +6,36 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    let layer = CALayer()
     var body: some View {
         VStack {
-         
             Text("Hello, world!")
+            Board()
         }
         .padding()
     }
 }
 
+
+struct Board:UIViewRepresentable {
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+
+        print("hello")
+    }
+    
+    
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: CGRect(x:0,y:0,width: 200,height: 200))
+        let layer = CALayer()
+        layer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        layer.backgroundColor = UIColor.red.cgColor
+        view.layer.addSublayer(layer)
+        return view
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
