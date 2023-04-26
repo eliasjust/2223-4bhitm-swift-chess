21aa2165f5b374b7ce393a173fcb88cce1578056
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2023-04-26 11:33:05 +0200

    optimize redraw

--- code/Sources/BoardView.swift
+++ code/Sources/BoardView.swift
@@ -17,7 +17,6 @@ class BoardView: UIView {
     
     let boardSize: CGFloat = UIScreen.main.bounds.width * 0.9
     var squareSize: CGFloat { boardSize  / 8 }
-    var offset:CGFloat  = 0
     let whiteColor = UIColor.lightGray.cgColor
     let blackColor = UIColor.darkGray.cgColor
     let activeColor = UIColor.yellow.cgColor
@@ -28,18 +27,13 @@ class BoardView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     fileprivate func drawPieces() {
-        layer.sublayers?[0].setNeedsDisplay()
-        layer.setNeedsLayout()
+        
         for (i, row) in viewmodel.board.enumerated() {
             for (j, piece) in row.enumerated() {
                 if  piece != nil {
-                    
-                    let pieceLayer = createPieceLayer(piece: piece?.chessPiece, color: piece?.chessColor)
-                    //let pieceLayer = createPieceLayerPaint(color: piece?.chessColor)
-                    
-                    drawPiece(xPos: CGFloat(j), yPos: CGFloat(i), piece: pieceLayer)
-                    
-                    layer.addSublayer(pieceLayer)
+                    subviews[i].subviews[j].subviews.forEach{$0.removeFromSuperview()}
+                    let pieceView = createPieceLayer(piece: piece?.chessPiece, color: piece?.chessColor)
+                    subviews[i].subviews[j].addSubview(pieceView)
                     
                 }
             }
@@ -47,49 +41,15 @@ class BoardView: UIView {
     }
     
     
-    /* func createPieceLayerPaint(color: Model.ChessColor?) -> CALayer {
-     let pieceLayer = CAShapeLayer()
-     let kingPath = UIBezierPath()
-     
-     
-     kingPath.move(to: CGPoint(x: squareSize / 2, y: squareSize / 4))
-     kingPath.addLine(to: CGPoint(x: squareSize / 2, y: 3 * squareSize / 4))
-     kingPath.move(to: CGPoint(x: squareSize / 4, y: squareSize / 2))
-     kingPath.addLine(to: CGPoint(x: 3 * squareSize / 4, y: squareSize / 2))
-     
-     pieceLayer.path = kingPath.cgPath
-     pieceLayer.lineWidth = 3
-     
-     if color == .white {
-     pieceLayer.strokeColor =
-     UIColor.white.cgColor
-     
-     } else {
-     pieceLayer.strokeColor =
-     UIColor.green.cgColor
-     }
-     
-     
-     pieceLayer.fillColor = UIColor.clear.cgColor
-     return pieceLayer
-     }*/
     
     
+
     
-    
- 
-    func drawPiece(xPos: Double,yPos: Double ,piece: CALayer) {
+    func createPieceLayer(piece: Model.ChessPiece?, color: Model.ChessColor?) -> UIView {
+        let pieceView = UIView()
         
-        piece.setAffineTransform(
-            CGAffineTransform(translationX: squareSize * xPos + offset, y: squareSize * yPos )
-        )
-    }
-    
-    func createPieceLayer(piece: Model.ChessPiece?, color: Model.ChessColor?) -> CALayer {
-        
-        let pieceLayer = CALayer()
-        pieceLayer.bounds = CGRect(x:0,y:0,width:squareSize,height: squareSize)
-        pieceLayer.position = CGPoint(x: squareSize / 2, y: squareSize / 2)
+        pieceView.layer.frame = CGRect(x:0,y:0,width:squareSize,height: squareSize)
+       
         
         let imageName: String
         switch (piece, color) {
@@ -122,13 +82,15 @@ class BoardView: UIView {
         }
         
         if let image = UIImage(named: imageName) {
-            pieceLayer.contents = image.cgImage
+            pieceView.layer.contents = image.cgImage
         }
         
-        pieceLayer.contentsGravity = .resizeAspect
+        pieceView.layer.contentsGravity = .resizeAspect
         
-        return pieceLayer
+        return pieceView
     }
+    
+    
     
     
     
@@ -141,51 +103,45 @@ class BoardView: UIView {
             let rowView = UIView()
             rowView.layer.frame = CGRect(x: 0 , y:   CGFloat(row) * squareSize, width: rowWidth, height: squareSize)
             self.addSubview(rowView)
-          
-
+            
+            
             for col in 0..<8 {
                 let colView = UIView()
                 let x = CGFloat(col) * squareSize
-        
+                
                 colView.layer.frame = CGRect(x: x , y: 0, width: squareSize, height: squareSize)
                 let color  = (row + col) % 2 == 0 ? whiteColor  : blackColor
                 colView.layer.backgroundColor = color
                 rowView.addSubview(colView)
                 let tapGeseture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
                 colView.addGestureRecognizer(tapGeseture)
-            
+                
                 
             }
         }
-       
-     
+        
+        
         
         
     }
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
-        print("called")
         if let colView = sender.view,
            let rowView = colView.superview
-          {
+        {
             let rowIndex = subviews.firstIndex(of: rowView)
-
             let colIndex = rowView.subviews.firstIndex(of: colView)
+            
             viewmodel.handleMove(data: ViewModel.Coordinates(row: rowIndex!, column: colIndex!))
-            print(viewmodel.fromPosition)
-            print(viewmodel.toPosition)
-            if viewmodel.fromPosition != nil {
+            let fromPosition = viewmodel.fromPosition
+            let toPosition = viewmodel.toPosition
+            if fromPosition != nil {
                 
-                if viewmodel.toPosition != nil {
-                   let row = viewmodel.fromPosition!.row
-                    let col = viewmodel.fromPosition!.column
-                    subviews[row].subviews[col].backgroundColor = viewmodel.previousColor
+                if toPosition != nil {
+
+                    self.setNeedsDisplay()
+                    self.viewmodel.clearValues()
                     
-           
-                    self.setNeedsDisplay(self.layer.frame)
-                    viewmodel.clearValues()
-                   
                 } else {
-                    viewmodel.previousColor =  colView.backgroundColor
                     colView.backgroundColor = UIColor(cgColor:activeColor)
                 }
             }
@@ -198,20 +154,29 @@ class BoardView: UIView {
             print(sender.view?.superview ??  "super view does not exist")
             print("not found")
         }
-     
+        
     }
     
     
-    
+    override func draw(_ rect:CGRect) {
+        
+        subviews.forEach{$0.removeFromSuperview()}
+        drawBoard()
+        drawPieces()
+        
+        
+    }
     
     init(viewModel:ViewModel) {
         self.viewmodel = viewModel
         super.init(frame: CGRect.zero
         )
-        super.frame =  CGRect(x: offset, y: 0, width: boardSize, height: boardSize)
+        super.frame =  CGRect(x: 0, y: 0, width: boardSize, height: boardSize)
         
-        drawBoard()
-        drawPieces()
+        
+        
+        
+        
         
     }
     
--- code/Sources/ContentView.swift
+++ code/Sources/ContentView.swift
@@ -13,9 +13,9 @@ struct ContentView: View {
     var body: some View {
       
         VStack {
-            BoardViewWrapper(viewmodel: viewmodel)
+            BoardViewWrapper(viewmodel: viewmodel).padding()
             
-        }
+        }.padding()
         
     }
 }
@@ -35,160 +35,7 @@ struct BoardViewWrapper: UIViewRepresentable {
 
 
 
-    
-    
-    struct BordComposition: UIViewRepresentable {
-        var viewmodel: ViewModel
-        var parentSize: CGSize
-        var boardSize: CGFloat { min(parentSize.width, parentSize.height) * 0.9 }
-        var squareSize: CGFloat { boardSize / 8 }
-        var offset: CGFloat { (parentSize.width - boardSize) / 2 }
-        let whiteColor = UIColor.gray.cgColor
-        let blackColor = UIColor.black.cgColor
-        let activeColor = UIColor.yellow.cgColor
-        
-        
-        
-        fileprivate func drawPieces(_ view: UIView) {
-            for (i, row) in viewmodel.board.enumerated() {
-                for (j, piece) in row.enumerated() {
-                    if  piece != nil {
-                        
-                        //let pieceLayer = createPieceLayer(piece: piece?.chessPiece, color: piece?.chessColor)
-                        let pieceLayer = createPieceLayerPaint(color: piece?.chessColor)
-                        
-                        drawPiece(xPos: CGFloat(j), yPos: CGFloat(i), piece: pieceLayer)
-                        
-                        view.layer.addSublayer(pieceLayer)
-                        
-                    }
-                }
-            }
-        }
-        
-        
-        func makeUIView(context: Context) -> UIView {
-            let view = UIView(frame: CGRect(x: offset, y: 0, width: boardSize, height: boardSize))
-            
-            
-            
-            
-            
-            
-            //let layerSize = view.layer.bounds.width
-            //let transform = CGAffineTransform(translationX: view.layer.bounds.width / 2, y:
-            
-            view.layer.setAffineTransform(CGAffineTransform(translationX: boardSize/2, y: boardSize/2))
-            
-            
-            drawBoard(view)
-            drawPieces(view)
-            return view
-        }
-        
-        
-        
-        func updateUIView(_ uiView: UIView, context: Context) {
-            
-            
-        }
-        
-        
-       
-        
-        
-        
-        func drawPiece(xPos: Double,yPos: Double ,piece: CALayer) {
-            
-            piece.setAffineTransform(
-                CGAffineTransform(translationX: squareSize * xPos + offset, y: squareSize * yPos )
-            )
-        }
-        
-        func createPieceLayer(piece: Model.ChessPiece?, color: Model.ChessColor?) -> CALayer {
-            let pieceLayer = CALayer()
-            
-            pieceLayer.bounds = CGRect(x: 0, y: 0, width: squareSize, height: squareSize)
-            
-            //        pieceLayer.position = CGPoint(x: squareSize / 2, y: squareSize / 2)d
-            
-            let imageName: String
-            switch (piece, color) {
-            case (.king, .white):
-                imageName = "white_king"
-            case (.king, .black):
-                imageName = "yeah"
-            default:
-                imageName = ""
-            }
-            
-            if let image = UIImage(named: imageName) {
-                pieceLayer.contents = image.cgImage
-            }
-            
-            pieceLayer.contentsGravity = .resizeAspect
-            
-            return pieceLayer
-        }
-        
-        func createPieceLayerPaint(color: Model.ChessColor?) -> CALayer {
-            let pieceLayer = CAShapeLayer()
-            let kingPath = UIBezierPath()
-            
-            
-            kingPath.move(to: CGPoint(x: squareSize / 2, y: squareSize / 4))
-            kingPath.addLine(to: CGPoint(x: squareSize / 2, y: 3 * squareSize / 4))
-            kingPath.move(to: CGPoint(x: squareSize / 4, y: squareSize / 2))
-            kingPath.addLine(to: CGPoint(x: 3 * squareSize / 4, y: squareSize / 2))
-            
-            pieceLayer.path = kingPath.cgPath
-            pieceLayer.lineWidth = 3
-            
-            if color == .white {
-                pieceLayer.strokeColor =
-                UIColor.white.cgColor
-                
-            } else {
-                pieceLayer.strokeColor =
-                UIColor.green.cgColor
-            }
-    
-            
-            pieceLayer.fillColor = UIColor.clear.cgColor
-  
-            pieceLayer.fillColor = UIColor.clear.cgColor
-            return pieceLayer
-        }
-       
-        
-        func drawBoard(_ view: UIView) {
-            //king.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-            //king.transform = CGAffineTransform(scaleX: 2, y: 2)
-            //king.setAffineTransform(CGAffineTransform(translationX: 0, y: 0))
-            let  squareSize =  view.layer.bounds.width / 8
-            for row in 0..<8 {
-                for col in 0..<8 {
-                    
-                    let squareLayer = CALayer()
-                    let frame = CGRect(x: CGFloat(col) * squareSize + offset , y: CGFloat(row) * squareSize, width: squareSize, height: squareSize)
-                    squareLayer.frame = frame
-                    let color =  (row + col) % 2 == 0 ? whiteColor : blackColor
-                    
-                    squareLayer.backgroundColor = color
-                    squareLayer.backgroundColor = viewmodel.fromPosition != nil ? activeColor : squareLayer.backgroundColor
-                    
-                    view.layer.addSublayer(squareLayer)
-                }
-            }
-            
-            
-        }
-            
-        
-        
-        
-        
-    }
+
     
     
     
